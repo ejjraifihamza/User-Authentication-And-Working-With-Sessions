87dbada04fd510e962cc1958315b5ac0261a7415
Hamza ejjraifi <76843028+ejjraifihamza@users.noreply.github.com>
2022-01-22 19:11:16 +0000

    🚀 Adding_Basic_SignUp_Fonctionality

--- routes/demo.js
+++ routes/demo.js
@@ -1,29 +1,38 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('../data/database');
+const db = require("../data/database");
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
-  res.render('welcome');
+router.get("/", function (req, res) {
+  res.render("welcome");
 });
 
-router.get('/signup', function (req, res) {
-  res.render('signup');
+router.get("/signup", function (req, res) {
+  res.render("signup");
 });
 
-router.get('/login', function (req, res) {
-  res.render('login');
+router.get("/login", function (req, res) {
+  res.render("login");
 });
 
-router.post('/signup', async function (req, res) {});
-
-router.post('/login', async function (req, res) {});
-
-router.get('/admin', function (req, res) {
-  res.render('admin');
+router.post("/signup", async function (req, res) {
+  const userData = req.body;
+  const confirmEmail = userData["confirm-email"];
+  const { email, password } = userData;
+  await db.getDb().collection("users").insertOne({
+    email: email,
+    password: password,
+  });
+  res.redirect("/login");
 });
 
-router.post('/logout', function (req, res) {});
+router.post("/login", async function (req, res) {});
+
+router.get("/admin", function (req, res) {
+  res.render("admin");
+});
+
+router.post("/logout", function (req, res) {});
 
 module.exports = router;
